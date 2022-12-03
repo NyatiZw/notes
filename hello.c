2cbcb8c3e43be49a2227d0ac36d4bbfa43e684dc
NyatiZw <appsmaid19@gmail.com>
2022-12-03 06:47:12 -0800

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(void) {
+	
+	char * a = "Simba";
+	int age = 35;
+
+	printf("\nHello there\n");
+	printf("%s\n", a);
+	printf("%d\n", age);
+	printf("%s\n", "");
+}
+
